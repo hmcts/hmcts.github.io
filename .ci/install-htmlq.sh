0f6d971fc0937914565635f2d027b8c323628455
Tim Jacomb <timjacomb1@gmail.com>
2024-08-07 14:47:13 +0100

    Remove sudo from install command

--- .ci/install-htmlq.sh
+++ .ci/install-htmlq.sh
@@ -2,5 +2,5 @@
 
 wget https://github.com/mgdm/htmlq/releases/download/v0.4.0/htmlq-x86_64-linux.tar.gz
 tar -xzf htmlq-x86_64-linux.tar.gz
-sudo install htmlq /usr/bin/htmlq
+install htmlq /usr/bin/htmlq
 rm -f htmlq-x86_64-linux.tar.gz
