c26c6b36efacffca17100633e518ae3163571112
Tim Jacomb <timjacomb1@gmail.com>
2024-08-07 15:34:52 +0100

    Try this

--- .ci/install-htmlq.sh
+++ .ci/install-htmlq.sh
@@ -2,5 +2,10 @@
 
 wget https://github.com/mgdm/htmlq/releases/download/v0.4.0/htmlq-x86_64-linux.tar.gz
 tar -xzf htmlq-x86_64-linux.tar.gz
-sudo install htmlq /usr/bin/htmlq
+
+if [ command -v sudo &> /dev/null ]; then
+  sudo install htmlq /usr/bin/htmlq
+else
+  install htmlq /usr/bin/htmlq
+fi
 rm -f htmlq-x86_64-linux.tar.gz
